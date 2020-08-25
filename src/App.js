74f98f6d106daf069f0c3eac46deb42343ca74ad
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-08-25 10:48:10 -0700

    initial commit sidebar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+	return (
+		<div className="app">
+			<h1>hello there</h1>
+			<Sidebar />
+		</div>
+	);
+}
+
+export default App;
